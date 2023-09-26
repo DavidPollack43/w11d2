96e77944f20dcf7b4a578b5b59c756fb522b4708
David <2018dpollack@gmail.com>
2023-09-26 12:25:42 -0700

    Phase 1

--- practice-for-week-14-react-context-greenhouse-long-practice-main/src/components/Greenhouse/Greenhouse.js
+++ practice-for-week-14-react-context-greenhouse-long-practice-main/src/components/Greenhouse/Greenhouse.js
@@ -1,18 +1,22 @@
 import dayImage from './images/greenhouse-day.jpg';
 import nightImage from './images/greenhouse-night.jpg';
 import './Greenhouse.css';
+import { useTheme } from '../../context/ThemeContext';
 
 import LightSwitch from './LightSwitch';
 import ClimateStats from './ClimateStats';
 
 function Greenhouse() {
+  const {themeName, setThemeName} = useTheme();
 
+  let imageName = themeName === "day" ? dayImage : nightImage
   return (
     <section>
       <img  className='greenhouse-img'
-            src={dayImage}
+            src={imageName} //`${themeName}Image`
             alt='greenhouse' 
       />
+      {console.log(`${themeName}Image`)}
       <LightSwitch />
       <ClimateStats />
     </section>
--- practice-for-week-14-react-context-greenhouse-long-practice-main/src/index.js
+++ practice-for-week-14-react-context-greenhouse-long-practice-main/src/index.js
@@ -6,6 +6,7 @@ import App from './App';
 import ThemeProvider from './context/ThemeContext';
 
 function Root() {
+  
   return (
       <ThemeProvider>
         <BrowserRouter>
